d26ee00add64deab68bb11c425f490c9dcc6bfd1
Kevin Spaeth <kevin@createhaus.com>
2012-08-25 16:26:07 -0400

    Rename main.xml

--- RoadAngel/src/com/team06/roadangel/RoadAngelActivity.java
+++ RoadAngel/src/com/team06/roadangel/RoadAngelActivity.java
@@ -20,7 +20,7 @@ public class RoadAngelActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.register);
 
         openDataSource();
 
