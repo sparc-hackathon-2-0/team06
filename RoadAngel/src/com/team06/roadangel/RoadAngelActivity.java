a051d905c661524ed2145d5cb742177cb7309d2c
ericwood73 <ericwood73@gmail.com>
2012-08-25 10:12:34 -0400

    initial commit

---
+++ RoadAngel/src/com/team06/roadangel/RoadAngelActivity.java
@@ -0,0 +1,15 @@
+package com.team06.roadangel;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RoadAngelActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
