573841f9a073a68d490a8bba0db0995ce4d0f22b
ericwood73 <ericwood73@gmail.com>
2012-08-25 14:24:41 -0400

    initial commit

--- RoadAngel/src/com/team06/roadangel/RoadAngelActivity.java
+++ RoadAngel/src/com/team06/roadangel/RoadAngelActivity.java
@@ -1,6 +1,9 @@
 package com.team06.roadangel;
 
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class RoadAngelActivity extends Activity {
@@ -11,5 +14,10 @@ public class RoadAngelActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        // Make sure the AlertNotificationService is started
+        Intent intent = new Intent(this, AlertNotificationService.class);
+        intent.getExtras().putString(getString(R.string.server_tag_varname), "XYZ");
+        startService(intent);
     }
 }
